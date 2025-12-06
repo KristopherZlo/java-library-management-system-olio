51c02937916d55e816a6a1f3cc1474d84956697f
KristopherZlo <kostos2001@gmail.com>
2025-12-06 12:35:00 +0200

    refine storage exception, add missing logic, remove temp notes

--- src/main/java/lms/exception/StorageException.java
+++ src/main/java/lms/exception/StorageException.java
@@ -1,11 +1,11 @@
 package lms.exception;
-// TODO: add error codes
-// TODO: review message wording
 
 public class StorageException extends RuntimeException {
     public StorageException(String message, Throwable cause) {
+        super(message, cause);
     }
 
     public StorageException(String message) {
+        super(message);
     }
 }
