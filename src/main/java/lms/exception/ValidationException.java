38c048c72469c0962451df5bda0d2fc4cecbb6e6
KristopherZlo <kostos2001@gmail.com>
2025-12-06 13:50:00 +0200

    refine validation exception, add missing logic, remove temp notes

--- src/main/java/lms/exception/ValidationException.java
+++ src/main/java/lms/exception/ValidationException.java
@@ -1,8 +1,7 @@
 package lms.exception;
-// TODO: add error codes
-// TODO: review message wording
 
 public class ValidationException extends RuntimeException {
     public ValidationException(String message) {
+        super(message);
     }
 }
