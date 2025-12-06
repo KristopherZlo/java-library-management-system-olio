3ec6dec2b6d0fc4b7f5ba99c41ec0c028e254a46
KristopherZlo <kostos2001@gmail.com>
2025-12-06 10:05:00 +0200

    refine not found exception, add missing logic, remove temp notes

--- src/main/java/lms/exception/NotFoundException.java
+++ src/main/java/lms/exception/NotFoundException.java
@@ -1,8 +1,7 @@
 package lms.exception;
-// TODO: add error codes
-// TODO: review message wording
 
 public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
+        super(message);
     }
 }
