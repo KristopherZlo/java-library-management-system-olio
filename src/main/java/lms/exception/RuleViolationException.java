353068e8af772b3c05dac287bec1de0ac89a18cb
KristopherZlo <kostos2001@gmail.com>
2025-12-06 11:20:00 +0200

    refine rule violation exception, add missing logic, remove temp notes

--- src/main/java/lms/exception/RuleViolationException.java
+++ src/main/java/lms/exception/RuleViolationException.java
@@ -1,8 +1,7 @@
 package lms.exception;
-// TODO: add error codes
-// TODO: review message wording
 
 public class RuleViolationException extends RuntimeException {
     public RuleViolationException(String message) {
+        super(message);
     }
 }
