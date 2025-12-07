248188f7481cf5d516883344a108379536aad090
KristopherZlo <kostos2001@gmail.com>
2025-12-07 13:50:00 +0200

    refine librarian, add missing logic, remove temp notes

--- src/main/java/lms/model/Librarian.java
+++ src/main/java/lms/model/Librarian.java
@@ -1,15 +1,15 @@
 package lms.model;
-// TODO: review field validation
-// TODO: add helper methods
 
 public class Librarian extends User {
     public Librarian() {
     }
 
     public Librarian(String id, String name, String email) {
+        super(id, name, email);
     }
 
     @Override
     public boolean canManageLibrary() {
+        return true;
     }
 }
