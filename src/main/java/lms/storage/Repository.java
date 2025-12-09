8c9b77527cccaadd0579c8694ac9d4c989507167
KristopherZlo <kostos2001@gmail.com>
2025-12-09 13:50:00 +0200

    refine repository, add missing logic, remove temp notes

--- src/main/java/lms/storage/Repository.java
+++ src/main/java/lms/storage/Repository.java
@@ -1,13 +1,13 @@
 package lms.storage;
-// TODO: review transaction flow
-// TODO: add metrics hooks
 
 import java.util.List;
 import java.util.Optional;
 import lms.model.Identifiable;
 
 public interface Repository<T extends Identifiable<ID>, ID> {
+    void save(T entity);
 
+    Optional<T> findById(ID id);
 
     List<T> findAll();
 
