3952fb55ca1520d8d0bc760f4bb4800214854e23
KristopherZlo <kostos2001@gmail.com>
2025-12-11 15:05:00 +0200

    add date provider, add system clock

---
+++ src/main/java/lms/util/DateProvider.java
@@ -0,0 +1,8 @@
+package lms.util;
+// TODO: review edge cases
+// TODO: add test notes
+
+import java.time.LocalDate;
+
+public interface DateProvider {
+}
---
+++ src/main/java/lms/util/SystemDateProvider.java
@@ -0,0 +1,11 @@
+package lms.util;
+// TODO: review edge cases
+// TODO: add test notes
+
+import java.time.LocalDate;
+
+public class SystemDateProvider implements DateProvider {
+    @Override
+    public LocalDate today() {
+    }
+}
