216065fea819e1d76d646c24fc12e8c66bb480a1
KristopherZlo <kostos2001@gmail.com>
2025-12-28 10:05:00 +0200

    refine adult loan policy, add missing logic, remove temp notes

--- src/main/java/lms/policy/AdultLoanPolicy.java
+++ src/main/java/lms/policy/AdultLoanPolicy.java
@@ -1,15 +1,15 @@
 package lms.policy;
-// TODO: revisit rules
-// TODO: expose configuration
 
 import lms.model.Member;
 
 public class AdultLoanPolicy implements LoanPolicy {
     @Override
     public int loanDays(Member member) {
+        return 21;
     }
 
     @Override
     public int maxLoans(Member member) {
+        return 3;
     }
 }
