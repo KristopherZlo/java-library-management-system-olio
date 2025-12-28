a6f89e14c2524255d45ab40e0e3d6db0b88e2369
KristopherZlo <kostos2001@gmail.com>
2025-12-28 11:20:00 +0200

    refine fine policy, add missing logic, remove temp notes

--- src/main/java/lms/policy/FinePolicy.java
+++ src/main/java/lms/policy/FinePolicy.java
@@ -1,9 +1,8 @@
 package lms.policy;
-// TODO: revisit rules
-// TODO: expose configuration
 
 import lms.model.Loan;
 import java.time.LocalDate;
 
 public interface FinePolicy {
+    long fineCents(Loan loan, LocalDate today);
 }
