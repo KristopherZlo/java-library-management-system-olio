3edc8d16abbd82b4c47b83f36eddbe07f8b00553
KristopherZlo <kostos2001@gmail.com>
2025-12-28 12:35:00 +0200

    refine no fine policy, add missing logic, remove temp notes

--- src/main/java/lms/policy/NoFinePolicy.java
+++ src/main/java/lms/policy/NoFinePolicy.java
@@ -1,6 +1,4 @@
 package lms.policy;
-// TODO: revisit rules
-// TODO: expose configuration
 
 import lms.model.Loan;
 import java.time.LocalDate;
@@ -8,5 +6,6 @@ import java.time.LocalDate;
 public class NoFinePolicy implements FinePolicy {
     @Override
     public long fineCents(Loan loan, LocalDate today) {
+        return 0;
     }
 }
